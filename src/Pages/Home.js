ed4b9bdcbb9f02e94dd4d47673f4cb1af26f49c0
sogatanco <adeliapucha46@gmail.com>
2021-06-07 14:04:15 +0700

    initialize project

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Wrapper/Navbar';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <Navbar></Navbar>
+        );
+    }
+}
+export default Home;
---
+++ src/Pages/Wrapper/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <p>Navbar</p>
+        )
+    }
+}
+export default Navbar;
