9b76d63d5d90a793af28b76dc1dbcc44db1cc090
sogatanco <adeliapucha46@gmail.com>
2021-06-13 23:09:13 +0700

    create login with phone number

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import firebaseConfig from '../firebaseConfig';
+import * as firebaseui from 'firebaseui';
+import firebase from 'firebase';
+import 'firebaseui/dist/firebaseui.css';
+
 
 class Login extends React.Component{
-    constructor(){
-        super();
+    componentDidMount(){
+        firebase.initializeApp(firebaseConfig);
+        const uiConfig={
+            signInOptions:[{
+                provider:firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                recaptchaParameters:{
+                    type:'image',
+                    size:'normal',
+                    badge:'bottomleft',
+                },
+                defaultCountry:'US',
+            }],
+            callbacks:{
+                signInSuccessWithAuthResult:function(authResult, redirectUrl){
+                    alert('succesfull');
+                    return true;
+                }
+            },
+            signInSuccessUrl:'https://promosteem.com'
+        }
 
+        var ui=new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start('#firebase-auth-container', uiConfig);
     }
+
+
 
     render(){
         return(
-            <p>login</p>
+            <div className="mt-4">
+                <div id="firebase-auth-container" >
+                
+                </div>
+            </div>
+            
         );
     }
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDliMWKflUfbcKJtsDFdcBK7N1zwlC51Hk",
+    authDomain: "bank-steem.firebaseapp.com",
+    projectId: "bank-steem",
+    storageBucket: "bank-steem.appspot.com",
+    messagingSenderId: "300425944534",
+    appId: "1:300425944534:web:7074f7c4911981cc9d1877"
+  };
+
+
+  export default firebaseConfig;
